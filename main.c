53ba4ffb075d8b643f3923ee76a9a4e59fb77e55
Wejknis <weja077@gmail.com>
2018-12-08 12:53:16 +0100

    1 2 3 start działa

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
+#include <stdlib.h>
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int liczba_slow, char * tablica_slow[]){
+    double wynik = 0.0;
+    double x;
+    double potega_x = 1.0;
+    int i;
+
+    x = atof(tablica_slow[1]);
+
+    for (i = 2; i < liczba_slow; ++i){
+        wynik += potega_x * atof(tablica_slow[i]);
+        potega_x *= x;
+    }
+
+        printf("Wynik: %lf\n", wynik);
     return 0;
 }
